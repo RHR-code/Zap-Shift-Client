61e11971f1908878c60ed609db70c061283edaf6
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-11-22 00:09:48 +0600

    react hook form added and firebase config file added

--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>this is the login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleRegistration = (data) => {
+    console.log("after submit", data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <fieldset className="fieldset">
+          {/* email */}
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input w-3/4"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+          {/* password */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/,
+            })}
+            className="input w-3/4"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 character or longer
+            </p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              The password should have at least one uppercase , one lowercase
+              and one digit
+            </p>
+          )}
+
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4 w-3/4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
